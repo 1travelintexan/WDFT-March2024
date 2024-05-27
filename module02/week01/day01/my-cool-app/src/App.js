c44ebced818d80ae2c7ad8f3098e2797fa73a224
Joshua George <horheyinc8@gmail.com>
2024-05-27 14:04:25 +0200

    vite intro

---
+++ module02/week01/day01/my-cool-app/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is super fun</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn at Ironhack
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
